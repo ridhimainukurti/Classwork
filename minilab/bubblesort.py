2d1fe1044db55834586bab51ca11fd6c0998c060
ryazhisai <73305760+ryazhisai@users.noreply.github.com>
2021-04-30 21:31:55 -0700

    isai bubble sort

---
+++ minilab/bubblesort.py
@@ -0,0 +1,17 @@
+def bubsort (input_lst):
+    n = len(input_lst)
+
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will repeat one time more than needed.
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if input_lst[j] > input_lst[j+1] :
+                input_lst[j], input_lst[j+1] = input_lst[j+1], input_lst[j]
+
+    return (input_lst)
